f5ac028b4d356a59ffc71ee34cbaaea322b412e3
ibraheemkabir <ibraheemkabir96@gmail.com>
2018-10-26 06:35:53 +0100

    UI design update

--- js/products.js
+++ js/products.js
@@ -31,6 +31,24 @@ const nav = document.querySelector('header');
     trigger2.addEventListener("click", toggleModal2);
     closeButton2.addEventListener("click", toggleModal2);
     window.addEventListener("click", windowOnClick2);
+
+    var modal3 = document.querySelector(".modal3");
+    var trigger3 = document.querySelector(".actives2");
+    var closeButton3 = document.querySelector(".close-buttons");
+
+    function toggleModal3() {
+        modal3.classList.toggle("show-modal");
+    }
+
+    function windowOnClick3(event) {
+        if (event.target === modal3) {
+            toggleModal3();
+        }
+    }
+
+    trigger3.addEventListener("click", toggleModal3);
+    closeButton3.addEventListener("click", toggleModal3);
+    window.addEventListener("click", windowOnClick3);
  
     (function(){
       /**
